2d5938c640afd0a7bfe3e49b102a014e945905dd
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2016-07-14 14:20:33 -0700

    Added Gun package and Classes
    tada

---
+++ src/main/java/com/mch/swat3/gun/GunBase.java
@@ -0,0 +1,12 @@
+package com.mch.swat3.gun;
+
+import com.mch.swat3.item.SWATItem;
+
+public class GunBase extends SWATItem{
+
+	public GunBase(String name, int maxStack) {
+		super(name, maxStack);
+	
+	}
+
+}
---
+++ src/main/java/com/mch/swat3/gun/PistonBase.java
@@ -0,0 +1,10 @@
+package com.mch.swat3.gun;
+
+public class PistonBase extends GunBase{
+
+	public PistonBase(String name, int maxStack) {
+		super(name, maxStack);
+
+	}
+
+}
