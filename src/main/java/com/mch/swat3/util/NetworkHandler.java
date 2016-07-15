f9054f02dc14d4359cf409149697fc669042d632
SeqSEE <sequoia.francois@gmail.com>
2016-07-14 22:31:14 -0700

    Added simple network handler

--- src/main/java/com/mch/swat3/util/NetworkHandler.java
+++ src/main/java/com/mch/swat3/util/NetworkHandler.java
@@ -1,5 +1,32 @@
 package com.mch.swat3.util;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class NetworkHandler {
+	
+	
+	/*
+	 * Returns true if can openConnection for google.com
+	 * 
+	 * Used for VersionHandler
+	 */
+    public static boolean networkAvailable(){
+    	try {
+    		final URL url = new URL("http://www.google.com");
+    		final URLConnection con = url.openConnection();
+    		con.connect();
+    		return true;
+    	} 
+    	catch (MalformedURLException e) {
+    		throw new RuntimeException(e);
+    		
+    	}
+    	catch (IOException e) {
+    		return false;
+    	}	    	
+    }
 
 }
