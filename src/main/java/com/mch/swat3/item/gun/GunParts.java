0b5f98648704b6d889ea49dca1da168744f41889
SeqSEE <sequoia.francois@gmail.com>
2016-07-15 00:26:09 -0700

    fix
    fixed extend from Items to Item

--- src/main/java/com/mch/swat3/item/gun/GunParts.java
+++ src/main/java/com/mch/swat3/item/gun/GunParts.java
@@ -1,7 +1,12 @@
 package com.mch.swat3.item.gun;
 
-import com.mch.swat3.init.SWATItems;
+import com.mch.swat3.item.SWATItem;
 
-public class GunParts extends SWATItems{
+public class GunParts extends SWATItem{
+
+	public GunParts(String name, int maxStack) {
+		super(name, maxStack);
+		// TODO Auto-generated constructor stub
+	}
 
 }
