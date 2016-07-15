5c393b4646910f38a219da7d2c5b1502e6dfa7c7
SeqSEE <sequoia.francois@gmail.com>
2016-07-15 00:28:27 -0700

    added gun ammo

--- src/main/java/com/mch/swat3/item/gun/GunAmmo.java
+++ src/main/java/com/mch/swat3/item/gun/GunAmmo.java
@@ -2,9 +2,9 @@ package com.mch.swat3.item.gun;
 
 import com.mch.swat3.item.SWATItem;
 
-public class GunParts extends SWATItem{
+public class GunAmmo extends SWATItem{
 
-	public GunParts(String name, int maxStack) {
+	public GunAmmo(String name, int maxStack) {
 		super(name, maxStack);
 	}
 
